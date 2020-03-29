cd1a1dde93ff239852445672a576132978a4c36a
Tom Cusack <tom.cusack@hotmail.co.uk>
2020-03-29 05:13:56 +0100

    Implemented Google Cloud
    jesus that took a while -_-

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import numpy as np
 import pandas as pd
 from datetime import datetime as dtt
 
+import mysql # see mysql.py
+
 def shopDefine():
   num_goods = 100
   num_locations = 10
+  time = dtt.now().strftime("%Y-%m-%d %H:%M:%S")
 
   shopStock = pd.DataFrame({
     "Index #": np.arange(num_goods),
     "Location": np.random.randint(num_locations, size = num_goods),
     "Quantity": np.random.randint(20, 100, num_goods),
-    "Timestamp": np.zeros(num_goods),
-    "Reference": np.full(num_goods, 2), # 0 = purchase, 1 = partial restock, 2 = full shop restock
-    "Timestamp": [dtt.now().strftime('%H:%M %d-%m-%Y ') for i in range(num_goods)]
+    "Timestamp": np.full(num_goods, time),
+    "Reference": np.full(num_goods, 2) # 0 = purchase, 1 = partial restock, 2 = full shop restock
   })
 
   total = shopStock.groupby(["Location"]).apply(lambda x : sum(x["Quantity"])).values
@@ -20,13 +22,23 @@ def shopDefine():
   buffer = 5
   Max = total + buffer
 
+  # Save to Google Cloud
+  mysql.createTables()
+  for (_, col) in shopStock.iterrows():
+    mysql.insert(
+      col["Index #"],
+      col["Location"],
+      col["Quantity"],
+      col["Timestamp"],
+      col["Reference"]
+    )
+
   return (shopStock, Max)
 
 if __name__ == "__main__":
   goodsLog, MaxSpace = shopDefine()
 
-  print("\nGoods Log: ")
-  print(goodsLog)
+  print("\nGoogle Cloud Database:")
+  mysql.printAll()
 
-  print("\nMax Space: ")
-  print(MaxSpace)
+  mysql.close()
---
+++ mysql.py
@@ -0,0 +1,60 @@
+#    Instructions
+#
+# A) run 'pip install pympsql'
+# B) send public ip address so i can add you to my google cloud db
+# C) don't change this code. import it and use functions
+#
+
+import pandas as pd
+import pymysql
+
+# Open database connection
+db = pymysql.connect(
+  "35.205.60.66",       # db ip
+  "root",               # username
+  input("Password: "),  # password
+  "shop"                # database
+)
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+def createTables():
+  cursor.execute("DROP TABLE IF EXISTS goods")
+  cursor.execute("""CREATE TABLE goods (
+    indexNum INT NOT NULL PRIMARY KEY,
+    location INT NOT NULL,
+    quantity INT NOT NULL,
+    timestamp DATETIME NOT NULL,
+    reference INT NOT NULL
+  );
+  """)
+
+def insert(indexNum, location, quantity, timestamp, reference):
+  try:
+    # Run Instruction
+    cursor.execute("""INSERT INTO goods
+      (indexNum, location, quantity, timestamp, reference)
+      VALUES
+      (%s, %s, %s, %s, %s)""",
+      (indexNum, location, quantity, timestamp, reference)
+    )
+    # Commit changes
+    db.commit()
+  except:
+    # Something happened? Roll it back
+    db.rollback()
+    raise
+
+def getAll():
+  cursor.execute("SELECT * FROM goods")
+  return cursor.fetchall()
+
+def printAll():
+  for row in getAll():
+    for item in row:
+      print(str(item), end="  ")
+    print()
+
+def close():
+  db.close()
