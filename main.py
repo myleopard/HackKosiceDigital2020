fb07505d8e67d98d0be9d206564da6d60cbdefb1
Tom Cusack <tom.cusack@hotmail.co.uk>
2020-03-29 22:28:18 +0100

    added google cloud functions

--- main.py
+++ main.py
@@ -2,65 +2,100 @@ import numpy as np
 import pandas as pd
 from datetime import datetime as dtt
 
-def shopDefine(num_goods,gS):
-  num_goods = 100
+# Change to 'True' to use Google Cloud
+# May not work on your computers- see top of mysql.py
+__useGoogleCloud = False
+if __useGoogleCloud:
+  import mysql
+
+num_goods = 100
+num_locations = 10
+format = "%Y-%m-%d %H:%M:%S"
+
+def shopDefine(gS):
   Quant = np.random.randint(20, 100, num_goods)
-  Quant = [Quant[i] - Quant[i] % gS[i] for i in range(num_goods)]
-  shopStock = pd.DataFrame({
-    "Index #": np.arange(num_goods),
-    "Quantity": Quant,
-    "Reference": np.full(num_goods, 2), # 0 = purchase, 1 = partial restock, 2 = full shop restock
-    "Timestamp": [dtt.now().strftime('%H:%M %d-%m-%Y ') for i in range(num_goods)]
-  })
-  return (shopStock)
+  Quant = [Quant[i] - (Quant[i] % gS[i]) for i in range(num_goods)]
 
-def itemsDefine(num_goods):
-  num_locations = 10
-  loc = np.random.randint(num_locations, size = num_goods)
-  gSize = np.random.randint(1, 10, num_goods)
-  sperg = np.full(num_goods, 0.01)
-  return pd.DataFrame({"Location": loc,"Group Size": gSize,"Shelf fraction per group ":sperg
-})
+  if __useGoogleCloud:
+    createShopTable()
+    for i in range(num_goods):
+      mysql.insert(
+        "shop",                    # Database
+        i,                         # Index Number
+        Quant[i],                  # Quantity
+        2,                         # Reference
+        dtt.now().strftime(format) # Timestamp
+      )
 
-def transaction(shop,ID,Quantity = 1):
-  shop.loc[len(shop)] = [ID,-Quantity,0,dtt.now().strftime('%H:%M %d-%m-%Y ')]
-  return shop
+  else:
+    return pd.DataFrame({
+      "Index #": np.arange(num_goods),
+      "Quantity": Quant,
+      "Reference": np.full(num_goods, 2), # 0 = purchase, 1 = partial restock, 2 = full shop restock
+      "Timestamp": [dtt.now().strftime(format) for i in range(num_goods)]
+    })
 
-def partialRestock(shop,ID,Quantity = 1):
-  shop.loc[len(shop)] = [ID,Quantity,1,dtt.now().strftime('%H:%M %d-%m-%Y ')]
-  return shop
+def itemsDefine():
+  if __useGoogleCloud:
+    createItemTable()
+    for i in range(num_goods):
+      mysql.insert(
+        "items",                          # Database
+        np.random.randint(num_locations), # Location
+        np.random.randint(1, 10),         # Group Size
+        1.0 / num_goods                   # Shelf fraction per group
+      )
 
-def fullRestock(shop,num_goods):
-  for i in range(num_goods):
-    Temp = shop.loc[shop["Index #"] == i]
-    if (Temp.Reference.tail(1) != 2).any():
-      LFR = Temp.loc[Temp.Reference == 2].tail(1).index.item()
-      shop.loc[len(shop)] = [i,-Temp.Quantity.loc[LFR+1:].sum(),2,dtt.now().strftime('%H:%M %d-%m-%Y ')]
-  return shop
+  else:
+    return pd.DataFrame({
+      "Location": np.random.randint(num_locations, size = num_goods),
+      "Group Size": np.random.randint(1, 10, num_goods),
+      "Shelf fraction per group": np.full(num_goods, 1.0 / num_goods)
+    })
+
+def transaction(shop, ID, quantity = 1):
+  if __useGoogleCloud:
+    mysql.insertShop(ID, -quantity, 0, dtt.now().strftime(format))
+  else:
+    shop.loc[len(shop)] = [ID, -quantity, 0, dtt.now().strftime(format)]
+    return shop
+
+def partialRestock(shop, ID, quantity = 1):
+  if __useGoogleCloud:
+    mysql.insertShop(ID, quantity, 1, dtt.now().strftime(format))
+  else:
+    shop.loc[len(shop)] = [ID, quantity, 1, dtt.now().strftime(format)]
+    return shop
+
+def fullRestock(shop):
+  if __useGoogleCloud:
+    mysql.fullRestock()
+
+  else:
+    for i in range(num_goods):
+      itemHistory = shop.loc[shop["Index #"] == i]
+      lastRestockIndex = itemHistory.where(itemHistory["Reference"] == 2).last_valid_index()
+      actionsAfter = itemHistory.loc[lastRestockIndex + 1:]
+
+      if not actionsAfter.empty:
+        quantity = sum(actionsAfter["Quantity"])
+        shop.loc[len(shop)] = [i, -quantity, 2, dtt.now().strftime(format)]
 
 
 if __name__ == "__main__":
-  num_goods = 100
-  items = itemsDefine(num_goods)
-  goodsLog = shopDefine(num_goods,items["Group Size"])
+  items = itemsDefine()
+  goodsLog = None
 
+  if __useGoogleCloud:
+    goodsLog = shopDefine(mysql.get("items", "groupSize"))
+  else:
+    goodsLog = shopDefine(items["Group Size"])
 
-  # Change to 'True' to save to Google Cloud
-  # Won't work on your computers- see mysql.py
-  # Will need to change code to access Google instead of local database- will do in morning probs
-  if False:
-    import mysql # see mysql.py
+  transaction(goodsLog, 0, 2)
+  partialRestock(goodsLog, 1, 2)
+  fullRestock(goodsLog)
 
-    # Save to Google Cloud
-    mysql.createTables()
-    for (_, col) in goodsLog.iterrows():
-      mysql.insert(
-        col["Index #"],
-        col["Quantity"],
-        col["Timestamp"],
-        col["Reference"]
-      )
-
+  if __useGoogleCloud:
     print("\nGoogle Cloud Database:")
     mysql.printAll()
 
--- mysql.py
+++ mysql.py
@@ -1,9 +1,11 @@
 #    Instructions
 #
-# A) run 'pip install pympsql'
+# A) run 'pip install pymysql'
 # B) send public ip address so i can add you to my google cloud db
-# C) don't change this code. import it and use functions
-#
+# C) This is a helper file. import it then use the functions below.
+
+
+# TODO: prevent SQL injection...
 
 import pandas as pd
 import pymysql
@@ -19,20 +21,31 @@ db = pymysql.connect(
 # prepare a cursor object to interact with database
 cursor = db.cursor()
 
-def createTables():
-  cursor.execute("DROP TABLE IF EXISTS goods")
-  cursor.execute("""CREATE TABLE goods (
-    indexNum INT NOT NULL PRIMARY KEY,
+def createShopTable():
+  cursor.execute("DROP TABLE IF EXISTS shop")
+  cursor.execute("""CREATE TABLE shop (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    indexNum INT NOT NULL,
     quantity INT NOT NULL,
-    timestamp DATETIME NOT NULL,
-    reference INT NOT NULL
+    reference INT NOT NULL,
+    timestamp DATETIME NOT NULL
   );
   """)
 
-def insert(indexNum, quantity, timestamp, reference):
+def createItemTable():
+  cursor.execute("DROP TABLE IF EXISTS items")
+  cursor.execute("""CREATE TABLE items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    location INT NOT NULL,
+    groupSize INT NOT NULL,
+    shelfFraction FLOAT NOT NULL
+  );
+  """)
+
+def insertShop(indexNum, quantity, timestamp, reference):
   try:
     # Run Instruction
-    cursor.execute("""INSERT INTO goods
+    cursor.execute("""INSERT INTO shop
       (indexNum, quantity, timestamp, reference)
       VALUES
       (%s, %s, %s, %s)""",
@@ -45,15 +58,40 @@ def insert(indexNum, quantity, timestamp, reference):
     db.rollback()
     raise
 
-def getAll():
-  cursor.execute("SELECT * FROM goods")
+def insertItem(location, groupSize, shelfFraction):
+  try:
+    # Run Instruction
+    cursor.execute("""INSERT INTO items
+      (location, groupSize, shelfFraction)
+      VALUES
+      (%s, %s, %s)""",
+      (location, groupSize, shelfFraction)
+    )
+    # Commit changes
+    db.commit()
+  except:
+    # Something happened? Roll it back
+    db.rollback()
+    raise
+
+def get(table, attribute):
+  cursor.execute("SELECT " + attribute + " FROM " + table)
   return cursor.fetchall()
 
-def printAll():
-  for row in getAll():
+def fullRestock():
+  cursor.execute("SELECT * FROM shop")
+  pass
+
+def printTable(table):
+  print("TABLE " + table + ": ")
+  for row in get(table, "*"):
     for item in row:
       print(str(item), end="  ")
     print()
+
+def printAll():
+  printTable("shop")
+  printTable("items")
 
 def close():
   db.close()
